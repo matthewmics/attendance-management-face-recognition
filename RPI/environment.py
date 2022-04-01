78ebf9fa8070138136fdc6f5dcafa2c536d74128
Matthew Miclat <38750461+matthewmics@users.noreply.github.com>
2022-04-01 19:53:43 +0800

    update comments

--- RPI/environment.py
+++ RPI/environment.py
@@ -1,10 +1,13 @@
+# SET API URL
 url = "http://127.0.0.1:5000"
 
 # threshold before face computation executes
 # minimum value = 10
+# setting this too high would take time before it captures, setting it too low would cause lag
+# fine tune this value until you get your desired result
 frame_threshold = 100
 
-#  0.45 = very strict, no look alikes would get captured 
+#  0.45 = very strict, no look alikes would get captured, set it to this when you have an HD web cam
 #  0.56 = still safe but look alikes could get captured
 similar_face_threshold = 0.51
 
